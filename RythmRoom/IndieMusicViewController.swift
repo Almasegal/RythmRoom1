628d9d7266e0478c142e3728102c32f454c7b470
Alma Segal <almasegal111@gmail.com>
2022-08-18 14:46:03 -0400

    errors

---
+++ RythmRoom/IndieMusicViewController.swift
@@ -0,0 +1,42 @@
+//
+//  IndieMusicViewController.swift
+//  RythmRoom
+//
+//  Created by Alma Segal on 2022-08-18.
+//
+
+import UIKit
+
+class IndieMusicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    var albums: = ["Lovers Rock", "Where'd All the Time Go?", "Tommy's Pool"]{
+
+    // shuffle in place
+        albums.shuffle();
+
+    // get a shuffled array back
+        let shuffled = albums.shuffled();
+}
+
+    if; let random = albums.randomElement()
+    {
+        print("Try: \(random).")
+    }
+        
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
