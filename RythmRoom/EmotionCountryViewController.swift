56095e2ce869c69814b7bce45f7c0a152ca44177
Alma Segal <almasegal111@gmail.com>
2022-08-19 11:02:06 -0400

    hi

---
+++ RythmRoom/EmotionCountryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmotionCountryViewController.swift
+//  RythmRoom
+//
+//  Created by Alma Segal on 2022-08-19.
+//
+
+import UIKit
+
+class EmotionCountryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
